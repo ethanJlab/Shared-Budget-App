e718386869510a347be391fa8d5badd838891988
ethanjanicki <ethanjanicki89@knights.ucf.edu>
2022-09-30 19:56:13 -0400

    ADD getAllUsers API boilerplate

--- api/routes/users.js
+++ api/routes/users.js
@@ -3,11 +3,47 @@
 const express = require('express');
 const router = express.Router();
 
+const MongoClient = require("mongodb").MongoClient;
+
+const uri = "mongodb+srv://tarova:jojothecat@cluster0.wy7ka4m.mongodb.net/?retryWrites=true&w=majority"
+
+const client = new MongoClient(uri);
+
+
+
 //const user = require('../../models/user');
 
-// @route GET api/users/test
-// @description tests books route
+// @route GET /users/test
+// @description tests route
 // @access Public
-router.get('/test', (req, res) => res.send('book route testing!'));
+router.get('/test', (req, res) => res.send('route testing!'));
+
+// @route GET /users/getAllUsers
+// @description get all users
+// @access Public
+router.get('/getAllUsers', (req, res) => {
+    async function run() {
+        var users = [];
+        try {
+            const database = client.db("Budget_app");
+            const users = database.collection("Users");
+            
+            // Query for all users
+            const query = {};
+            const cursor = users.find(query);
+            
+             usercursor.toArray();
+
+
+        } finally {
+            await client.close();
+        }
+        return users;
+    }
+    res.send(run());
+});
 
 module.exports = router;
+
+
+//mongodb+srv://tarova:<password>@cluster0.wy7ka4m.mongodb.net/?retryWrites=true&w=majority
