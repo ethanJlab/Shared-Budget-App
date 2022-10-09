23ed24f65f06628ff2864f4fdab5af3f7f4cbfa2
ethanjanicki <ethanjanicki89@knights.ucf.edu>
2022-10-08 23:17:21 -0400

    ADD dotenv requirments

--- api/config/db.js
+++ api/config/db.js
@@ -3,6 +3,8 @@
 const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const connectDB = async () => {
   try {
